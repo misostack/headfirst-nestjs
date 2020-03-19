15ff6eb03026c6836c9c5679857268bbd6246164
misostack <misostack.com@gmail.com>
2020-03-19 17:22:00 +0700

    Update api doc for user

--- src/user/models/dto/user.dto.ts
+++ src/user/models/dto/user.dto.ts
@@ -2,80 +2,51 @@ import {
   IsEmail, MinLength, MaxLength, IsNotEmpty, IsOptional
 } from 'class-validator';
 import { userLang } from '~/user/user.lang';
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
-  @IsEmail({}, {
-    message: userLang.validation.email.invalid
-  })
-  @MaxLength(320, {
-    message: '320'
-  })
+  @ApiProperty()
+  @IsEmail({}, { message: userLang.validation.email.invalid })
+  @MaxLength(320, { message: '320'})
   email: string;
 
-  @MinLength(6, {
-    message: '6'
-  })
-  @IsNotEmpty({
-    message: 'required'
-  })
+  @ApiProperty()
+  @MinLength(6, {message: '6'})
+  @IsNotEmpty({message: 'required'})
   password: string;
 
-  @MaxLength(60, {
-    message: '60'
-  })
-  @IsNotEmpty({
-    message: 'required'
-  })
+  @ApiProperty()
+  @MaxLength(60, {message: '60'})
+  @IsNotEmpty({message: 'required'})
   firstName: string;
 
-  @MaxLength(60, {
-    message: '60'
-  })
-  @IsNotEmpty({
-    message: 'required'
-  })  
+  @ApiProperty()
+  @MaxLength(60, {message: '60'})
+  @IsNotEmpty({message: 'required'})  
   lastName: string;
 }
 
 export class UpdateUserDto {
+  @ApiProperty()
   @IsOptional()
-  @IsEmail({}, {
-    message: userLang.validation.email.invalid
-  })
-  @MinLength(3, {
-    message: '3'
-  })
-  @MaxLength(320, {
-    message: '320'
-  })
+  @IsEmail({}, { message: userLang.validation.email.invalid })
+  @MaxLength(320, { message: '320'})
   email: string;
 
+  @ApiProperty()
   @IsOptional()
-  @MinLength(6, {
-    message: '6'
-  })
+  @MinLength(6, {message: '6'})
+  @IsNotEmpty({message: 'required'})
   password: string;
 
+  @ApiProperty()
   @IsOptional()
-  @MinLength(1, {
-    message: '1'
-  })
-  @MaxLength(60, {
-    message: '60'
-  })
-  @IsNotEmpty({
-    message: 'required'
-  })
+  @MaxLength(60, {message: '60'})
+  @IsNotEmpty({message: 'required'})
   firstName: string;
 
+  @ApiProperty()
   @IsOptional()
-  @MinLength(1, {
-    message: '1'
-  })
-  @MaxLength(60, {
-    message: '60'
-  })
-  @IsNotEmpty({
-    message: 'required'
-  })
+  @MaxLength(60, {message: '60'})
+  @IsNotEmpty({message: 'required'})  
   lastName: string;
 }
